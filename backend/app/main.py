87ce795d841b7f7f555be28b5997e1653502b5aa
yogesh944 <70305003+yogesh944@users.noreply.github.com>
2026-09-18 14:18:01 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -96,8 +96,9 @@ fastapi_app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://vercel.com/yogesh944s-projects/vakilo-demo-75le/5HtSMJsVAVaEKw3SMa42DxfTjnH4"
+        "https://vakilo-demo.vercel.app",
     ],
+    allow_origin_regex=r"https://vakilo-demo-75le-f82antxru-yogesh944s-projects.vercel.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
