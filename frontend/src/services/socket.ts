6f05752053a92c84a5e93e301583dbecabebac18
yogesh944 <70305003+yogesh944@users.noreply.github.com>
2026-09-18 13:21:57 +0530

    Change socket URL to production endpoint

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io, type Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://127.0.0.1:8000";
+const SOCKET_URL = "https://vakilo-demo-2.onrender.com/";
 
 let socket: Socket | null = null;
 
